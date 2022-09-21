c9ab674e022943b2425af57221c0788093bbb353
DiegoAlexUNALMED <112149672+DiegoAlexUNALMED@users.noreply.github.com>
2022-09-20 23:58:35 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -167,17 +167,11 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    import pandas as pd
-    tbl0 = pd.read_csv("https://raw.githubusercontent.com/ciencia-de-los-datos/programacion-usando-pandas-DiegoAlexUNALMED/main/tbl0.tsv", sep = "\t")
-    tbl0 = tbl0.sort_values(['_c1','_c2'])[['_c1','_c2']]
-    tbl0 = tbl0.groupby(by = '_c1').apply(lambda x: x.values)
-    tbl0 = tbl0.apply(lambda x: [str(xi[1]) for xi in x])
-    tbl0 = tbl0.to_frame()
-    tbl0[0] = tbl0[0].str.join(",").str.replace(",",":")
-    tbl0 = tbl0.rename(columns = {0:'_c0'})
-    tbl0.index.name = '_c1'
-    tbl0 = tbl0.reset_index()
-    return tbl0
+    df = tbl0.groupby('_c1')['_c2'].apply(list).reset_index()
+    df['_c2'] = df['_c2'].apply(lambda x: sorted(x))
+    df['_c2'] = df['_c2'].apply(lambda x: ':'.join(str(e) for e in x))
+    df.set_index("_c1", inplace = True)
+    return df
 
 
 def pregunta_11():
